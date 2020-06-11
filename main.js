d38293c25bc3b8648e307229b43f54a99a056698
zbarovsky <zachary.barovsky@gmail.com>
2020-06-11 11:47:03 -0700

    zombies spawn and move, but all to the bottom left corner. Fixed game space to look better. Set up collision detection for sprites and walls.

--- main.js
+++ main.js
@@ -3,27 +3,68 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {preload: preload, create: create, update: update})
 
 function preload() {
-    game.load.image('floor', 'img/flooring.png')
+    game.load.image('floor', 'img/flooringFinal.png')
     game.load.image('hero', 'img/x.png')
     game.load.image('zombie', 'img/zombie.png');
+    game.load.image('wallVert', 'img/wallVertActualFinal.png');
+    game.load.image('wallHorizontal', 'img/wallHorizontalFinal.png');
 }
 
-// Declare Group to attach sword to hero
+/* ----------------------- GROUPS HERE ------------------------ */
 
-// Declare Group that randomly spawns X number of zombies per room
-// randomX && randomY for positioning
-// Math.floor && Math.random for random amount of zombies
+let walls;
+let zombieHorde; 
+let hero;
+
 
 
 function create() {
     // startin up them physics
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
-    game.add.sprite(0, 0, 'floor');
+    let floor = game.add.sprite(0, 0, 'floor');
+    floor.scale.setTo(1, 1);
+
+    // set up walls grouping
+    walls = game.add.group();
+    walls.enableBody = true;
+
+    // top wall
+    let wallVert = walls.create(0, 0, 'wallVert');
+    wallVert.scale.setTo(1, 1);
+    wallVert.body.immovable = true;
+
+    // bottom wall
+    wallVert = walls.create (0, 575, 'wallVert')
+    wallVert.scale.setTo(1, 1);
+    wallVert.body.immovable = true;
+
+    // East Wall
+    let wallHorizontal = walls.create (750, 0, 'wallHorizontal');
+    wallHorizontal.scale.setTo(1, 1);
+    wallHorizontal.body.immovable = true;
+
+    // Weast wall
+    wallHorizontal = walls.create (-50, 0, 'wallHorizontal');
+    wallHorizontal.scale.setTo(1, 1);
+    wallHorizontal.body.immovable = true;
 
     // add hero and their physics
     player = game.add.sprite(375, 500, 'hero');
     game.physics.arcade.enable(player);
+    player.body.collideWorldBoundaires = true;
+
+    // Zombie Horde
+    zombieHorde = game.add.group();
+    zombieHorde.enableBody = true;
+    game.physics.arcade.enable(zombieHorde);
+
+    for (i = 0; i < Math.floor(Math.random() * 11); i++) {
+        let hoard = zombieHorde.create(game.world.randomX, game.world.randomY, 'zombie');
+        hoard.body.collideWorldBoundaires = true;
+        hoard.body.velocity.x = Math.floor(Math.random() * 100);
+        hoard.body.velocity.y = Math.floor(Math.random() * 100);
+    }
 
     // create keyboard management
     cursors = game.input.keyboard.createCursorKeys();
@@ -34,6 +75,10 @@ function create() {
 }
 
 function update() {
+
+    // physics for player and walls && hoard and walls
+    game.physics.arcade.collide(player, walls);
+    game.physics.arcade.collide(zombieHorde, walls);
 
     // lets give some movement to our hero
     player.body.velocity.x = 0;
@@ -54,8 +99,6 @@ function update() {
     } else {
         player.animations.stop();
     }
-
-
 
     // Center game on screen
     game.scale.pageAlignHorizontally = true;
