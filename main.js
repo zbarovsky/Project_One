fe21a70e6b371cce41207c0708d54b744550d41a
zbarovsky <zachary.barovsky@gmail.com>
2020-06-13 17:45:05 -0700

    multiple rooms designed. Next up is building Zombie boss and win function. Also, try and figure out sprites

--- main.js
+++ main.js
@@ -27,10 +27,12 @@ let floor;
 let walls;
 
 let doors;
+
 let northDoor;
 let southDoor;
 let eastDoor;
 let westDoor;
+
 
 let zombieHorde; 
 let player;
@@ -52,7 +54,10 @@ let finalScore;
 
 function create() {
     // startin up them physics
+    game.world.setBounds (0,0, 1600, 1200);
+
     game.physics.startSystem(Phaser.Physics.ARCADE);
+    
 
     createFloor()
 
@@ -60,7 +65,7 @@ function create() {
     walls = game.add.group();
     walls.enableBody = true;
 
-    createRoom()
+    createWalls()
 
     // Create doors and set up room change function
     doors = game.add.group()
@@ -86,17 +91,18 @@ function create() {
     // Create Score board and Lives Bar
     scoreBoard = game.add.text(16, 16, 'Score: 0', {fontSize: '32px', fill: '#ffffff'});
     lifeBar = game.add.text(600, 16, 'Life: 100', {fontSize: '32px', fill: '#ffffff'});
+
+    game.camera.follow(hero);
 }
 
 function update() {
 
     // physics for player and walls && hoard and walls
     game.physics.arcade.collide(hero, walls);
-    // game.physics.arcade.collide(hero, zombieHorde);
-
     game.physics.arcade.collide(sword, hero);
     game.physics.arcade.collide(sword, walls);
-    // game.physics.arcade.collide(sword, zombieHorde);
+    // game.physics.arcade.collide(hero, doors);
+    
 
     game.physics.arcade.collide(zombieHorde, walls);
 
@@ -150,7 +156,7 @@ function update() {
         //win();
     }
 
-    game.physics.arcade.overlap(player, zombieHorde, slayHero, null, this);
+    game.physics.arcade.overlap(hero, zombieHorde, slayHero, null, this);
 
     function slayHero (player, zombieHorde) {
         lives -=1;
@@ -173,73 +179,140 @@ function update() {
 /* ------------------- GAME FUNCTIONS ---------------------- */
 
 function createFloor () {
-    let floor = game.add.sprite(0, 0, 'floor');
-    floor.scale.setTo(1, 1);
-    } 
 
-function createRoom () {
+    let floor = game.add.sprite(0, 0, 'floor');
+    floor = game.add.sprite(800, 600, 'floor');
+    floor = game.add.sprite(800, 0, 'floor');
+    floor = game.add.sprite(0,600, 'floor');
+    floor.scale.setTo(1, 1);
+} 
+
+function createWalls () {
 
     // North wall
     let wallVert = walls.create(0, 0, 'wallVert');
     wallVert.scale.setTo(1, 1);
     wallVert.body.immovable = true;
-
-    // South wall
-    wallVert = walls.create (0, 575, 'wallVert')
+    // NorthWall part 2
+    wallVert = walls.create(750,0, 'wallVert');
     wallVert.scale.setTo(1, 1);
     wallVert.body.immovable = true;
 
-    // East Wall
+    // South wall
+    wallVert = walls.create (0, 1150, 'wallVert')
+    wallVert.scale.setTo(1, 1);
+    wallVert.body.immovable = true;
+    //south wall part 2
+    wallVert = walls.create (800, 1150, 'wallVert')
+    wallVert.scale.setTo(1, 1);
+    wallVert.body.immovable = true;
+
+    // Middle Horizontal wall
+    let wallMid = walls.create(0, 600, 'wallVert')
+    wallMid.scale.setTo(0.47, 1);
+    wallMid.body.immovable = true;
+    //Mid Horiz 2
+    wallMid = walls.create(450, 600, 'wallVert')
+    wallMid.scale.setTo(0.47, 1);
+    wallMid.body.immovable = true;
+    //Mid Horiz 3
+    wallMid = walls.create(800, 600, 'wallVert')
+    wallMid.scale.setTo(0.47, 1);
+    wallMid.body.immovable = true;
+    //mid Horiz 4
+    wallMid = walls.create(1240, 600, 'wallVert')
+    wallMid.scale.setTo(0.47, 1);
+    wallMid.body.immovable = true;
+
+    // Middle Vert Wall
     let wallHorizontal = walls.create (750, 0, 'wallHorizontal');
-    wallHorizontal.scale.setTo(1, 1);
+    wallHorizontal.scale.setTo(1, 0.65);
     wallHorizontal.body.immovable = true;
+    //Mid Vert 2
+    wallHorizontal = walls.create (750, 600, 'wallHorizontal');
+    wallHorizontal.scale.setTo(1, 0.5);
+    wallHorizontal.body.immovable = true;
+    //mid vert 3
+    wallHorizontal = walls.create (750, 1050, 'wallHorizontal');
+    wallHorizontal.scale.setTo(1, 0.5);
+    wallHorizontal.body.immovable = true;
+
+    // East Wall
+    wallMid = walls.create(1550,0, 'wallHorizontal');
+    wallMid.scale.setTo(1, 1.35);
+    wallMid.body.immovable = true;
 
     // Weast wall
     wallHorizontal = walls.create (-50, 0, 'wallHorizontal');
-    wallHorizontal.scale.setTo(1, 1);
+    wallHorizontal.scale.setTo(1, 2);
     wallHorizontal.body.immovable = true;
 
 }
 
 function createDoors() {
-    // North door
-    let northDoor = doors.create(375, 15, 'northDoor');
-    northDoor.body.immovable = true;
+
+    //Top Left
+    function roomOne () {
 
     // East door
-    let eastDoor = doors.create(740, 275, 'eastDoor');
+    let eastDoor = doors.create(750, 520, 'eastDoor');
+    eastDoor.scale.setTo(1,1.55);
+    eastDoor.enableBody = true;
     eastDoor.body.immovable = true;
 
     // South door
-    let southDoor = doors.create(375, 565, 'southDoor');
+    let southDoor = doors.create(380, 600, 'southDoor');
+    southDoor.scale.setTo(1.55,1);
+    southDoor.enableBody = true;
     southDoor.body.immovable = true;
 
-    // Weast door
-    let westDoor = doors.create(-30, 275, 'westDoor');
-    westDoor.body.immovable = true;
+    } roomOne()
 
+    // top right
+    function roomTwo() {
+        let southDoor = doors.create(1175, 600, 'southDoor');
+        southDoor.scale.setTo(1.5,1)
+        southDoor.enableBody = true;
+        southDoor.body.immovable = true; 
+    } roomTwo()
+
+    // bottom left
+    function roomThree() {
+        let eastDoor = doors.create(750, 1000, 'eastDoor');
+        eastDoor.enableBody = true;
+        eastDoor.body.immovable = true;
+    } roomThree()
+
+    // bottom right
+    function roomFour() {
+        let eastDoor = doors.create(1550, 1075, 'eastDoor');
+        eastDoor.scale.setTo(1,1.5);
+        eastDoor.enableBody = true;
+        eastDoor.body.immovable = true;
+    }roomFour()
 } 
 
 function createHero() {
 
-    hero = player.create(375, 500, 'hero');
+    hero = player.create(400, 300, 'hero');
     game.physics.arcade.enable(player);
     hero.body.collideWorldBoundaires = true;
 
     //sword to hero
-    sword = player.create(375, 475, 'sword');
+    sword = player.create(400, 275, 'sword');
     game.physics.arcade.enable(sword);
     sword.scale.setTo(.3, .3);
 } 
 
 function summonHoard() {
 
-    for (i = 0; i < Math.floor(Math.random() * 30); i++) {
+    for (i = 0; i < Math.floor(Math.random() * 100); i++) {
+
         let hoard = zombieHorde.create(game.world.randomX, game.world.randomY, 'zombie');
         hoard.body.collideWorldBoundaires = true;
+        // TODO change to follow player around if able
         game.physics.arcade.moveToXY(hoard, Math.floor(Math.random() * 400), Math.floor(Math.random() * 600), speed = 30);
 
-        //TODO Respawn function for different rooms
     }
 } 
 
@@ -255,10 +328,11 @@ function gameOver() {
     gameOverScreen.add(playAgainButton);
     gameOverScreen.add(playAgain);
     gameOverScreen.add(finalScore);
+
+    game.camera.follow(gameOverScreen);
 }
 
 function reset() {
-    //console.log("games reset dawg");
     
     gameOverScreen.visible =! gameOverScreen.visible;
     
@@ -270,6 +344,8 @@ function reset() {
     createHero()
     
     summonHoard()
+
+    game.camera.follow(hero);
 }
         
         
